0179d5055d5d3f7d1d8cc054b5d4bd621ddf3e9c
kishore <madishetty.kishore@gmail.com>
2018-10-03 01:04:24 +0530

    vaidations added for contactus

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contactus',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contactus.component.css']
 })
 export class ContactusComponent implements OnInit {
+  submitted:boolean=false;
+  contactusForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-  }
+    var emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+    this.contactusForm = this.fb.group({
+      type:['',[ Validators.required]],
+      description:['',[ Validators.required]],
+      name:['',[ Validators.required]],
+      email: ['', [Validators.required, Validators.pattern(emailPattern)]],
+      mobile: ['',[ Validators.required,Validators.minLength(10),Validators.maxLength(10)]] //, Validators.minLength(6)
+    });
+    this.contactusForm.controls['type'].setValue("Issue");
+
+  }
+  isNumberKey(evt) {
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+      return false;
+
+    return true;
+  }
+  get f() { return this.contactusForm.controls; }
+
+  savedata(contactusform) {
+    this.submitted = true;
+    if (this.contactusForm.valid) {
+
+    }else{
+      this.submitted = true;
+    }
+
+}
 }
--- src/app/userlogin/userlogin.component.ts
+++ src/app/userlogin/userlogin.component.ts
@@ -57,7 +57,7 @@ registrationsuccess:any="";
 
   ngOnInit() {
     this.disabledField = false;
-    var emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+    var emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     
     this.userloginForm = this.fb.group({
       username: ['', [Validators.required, Validators.pattern(emailPattern)]],
