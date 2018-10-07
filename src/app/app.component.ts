a6cbd5ad9888099fbc929339e42a32fbc3beed62
kishore <madishetty.kishore@gmail.com>
2018-10-08 00:16:22 +0530

    changes updated for uparrow showing when greater than 100px of scrolling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { UserloginComponent } from './userlogin/userlogin.component';
 import { UserregisterComponent } from './userregister/userregister.component';
 import { HomeComponent } from './home/home.component';
 import { Router, ActivatedRoute, NavigationEnd } from "@angular/router";
 import { SessionstorageService } from './services/sessionstorage.service';
-import{ AppConstants} from './services/constants'
+import { AppConstants } from './services/constants'
 import { EgazeService } from './services/egaze.service';
 
 @Component({
@@ -12,11 +12,11 @@ import { EgazeService } from './services/egaze.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'app';
-  user: Object={loginId: Number, email: String, role: String, status: String };
+  user: Object = { loginId: Number, email: String, role: String, status: String };
   flag: boolean = false;
-  user1:any;
+  user1: any;
   // navbarOpen = false;
   // toggleNavbar() {
   //   this.navbarOpen = !this.navbarOpen;
@@ -26,42 +26,52 @@ export class AppComponent implements OnInit{
   constructor(router: Router, route: ActivatedRoute, private sessionstorageService: SessionstorageService, private EgazeService: EgazeService) {
 
     this.routerProperty = router;
-    this.user =this.sessionstorageService.getUserDetails();
+    this.user = this.sessionstorageService.getUserDetails();
     if (this.user != null) {
-      this.user=JSON.parse(this.user+"");
-    //  alert(this.user)
+      this.user = JSON.parse(this.user + "");
+      //  alert(this.user)
       this.flag = true;
     }
   }
 
 
   userdashboard() {
-    if(this.sessionstorageService.getUserDetails()!=null){
-    this.user1 = JSON.parse(this.sessionstorageService.getUserDetails() + "");
-    this.EgazeService.getCustomerPackages(this.user1.loginId).subscribe(
-      result => {
-        if (Object.keys(result).length === 0) {
-          window.location.href = AppConstants.packageURL;
-        }else{
-          window.location.href = AppConstants.userdashboardURL;
+    if (this.sessionstorageService.getUserDetails() != null) {
+      this.user1 = JSON.parse(this.sessionstorageService.getUserDetails() + "");
+      this.EgazeService.getCustomerPackages(this.user1.loginId).subscribe(
+        result => {
+          if (Object.keys(result).length === 0) {
+            window.location.href = AppConstants.packageURL;
+          } else {
+            window.location.href = AppConstants.userdashboardURL;
+          }
         }
-      }
 
-    );
+      );
     }
   }
-  logout(){
+  logout() {
     this.sessionstorageService.removeUserDetails("user");
-    window.location.href=AppConstants.loginURL;
+    window.location.href = AppConstants.loginURL;
   }
 
   ngOnInit() {
     this.routerProperty.events.subscribe((evt) => {
-        if (!(evt instanceof NavigationEnd)) {
-            return;
-        }
-        window.scrollTo(0, 0)
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0)
     });
-}
-  
+  }
+
+  showUparrow: boolean = false;
+  @HostListener('window:scroll', ['$event'])
+  doSomething(event) {
+    if (window.pageYOffset > 100) {
+      this.showUparrow = true;
+    }
+    else {
+      this.showUparrow = false;
+    }
+  }
 }
